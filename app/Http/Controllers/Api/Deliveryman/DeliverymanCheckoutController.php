82940a4914759f3493c8202f0e15d9713017d2a6
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2016-05-06 00:11:38 -0300

    Checkout Deliveryman

--- app/Http/Controllers/Api/Deliveryman/DeliverymanCheckoutController.php
+++ app/Http/Controllers/Api/Deliveryman/DeliverymanCheckoutController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace CodeDelivery\Http\Controllers\Api\Client;
+namespace CodeDelivery\Http\Controllers\Api\Deliveryman;
 
 use CodeDelivery\Http\Controllers\Controller;
 use CodeDelivery\Repositories\OrderRepository;
@@ -10,17 +10,17 @@ use Illuminate\Http\Request;
 use LucaDegasperi\OAuth2Server\Facades\Authorizer;
 
 
-class ClientCheckoutController extends Controller
+class DeliverymanCheckoutController extends Controller
 {
     /**
      * @var OrderRepository
      */
     private $repository;
-    
     /**
      * @var UserRepository
      */
     private $userRepository;
+
 
     private $service;
 
@@ -38,33 +38,18 @@ class ClientCheckoutController extends Controller
     public function index()
     {
         $id = Authorizer::getResourceOwnerId();
-        $clientId = $this->userRepository->find($id)->client->id;
-        $orders = $this->repository->with('items')->scopeQuery(function($query) use($clientId) {
-            return $query->where('client_id', '=', $clientId);
+        $orders = $this->repository->with('items')->scopeQuery(function($query) use($id) {
+            return $query->where('user_deliveryman_id', '=', $id);
         })->paginate();
 
         return $orders;
     }
 
 
-    public function store(Request $request)
-    {
-        $data = $request->all();
-        $id = Authorizer::getResourceOwnerId();
-        $clientId = $this->userRepository->find($id)->client->id;
-        $data['client_id'] = $clientId;
-        $o = $this->service->create($data);
-        $o = $this->repository->with('items')->find($o->id);
-        return $o;
-    }
-
     public function show($id)
     {
-        $o = $this->repository->with(['client','items','cupom'])->find($id);
-        $o->items->each(function($item){
-            $item->product;
-        });
-        return $o;
+        $idDeliveryman = Authorizer::getResourceOwnerId();
+        return $this->repository->getByIdAndDeliveryman($id, $idDeliveryman);
     }
 
 }
